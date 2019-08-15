51277ecd16a575ed4b7ae0069bff5b5b1e96fa12
Darkpsychic <nairnikhil2001@gmail.com>
2019-08-15 16:14:28 +0530

    add colors and color codes for the tetrominoes

--- lib/Constant/Constant.rb
+++ lib/Constant/Constant.rb
@@ -11,4 +11,13 @@ module Constant
     BlockSize = 40
     NumOfBlocksX = ((Width + LineSize) / (BlockSize + LineSize)).round
     NumOfBlocksY = ((Height + LineSize) / (BlockSize + LineSize)).round
+
+    # the array has the following elements
+    # color_id, color
+    BoxColor = [1, [0, 0, 1, 0.5]]        #Blue
+    JedColor = [2, [0, 0.6, 0.4, 0.5]]    #Jade
+    LedColor = [3, [0.9, 0.9, 1, 0.5]]    #Lavender
+    StickColor = [4, [0, 0.1, 0.4, 0.5]]  #Sapphire
+    TeeColor = [5, [0, 0.5, 0.5, 0.5]]    #Teal
+    ZedColor = [6, [0.8, 0.7, 0.1, 0.5]]  #Zutem
 end
