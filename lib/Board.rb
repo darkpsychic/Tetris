7f0cb7c780c59ee0e05a3576b32a3c3f8f286726
Darkpsychic <nairnikhil2001@gmail.com>
2019-08-15 17:52:15 +0530

    implement colliding? and gameover?

--- lib/Board.rb
+++ lib/Board.rb
@@ -6,6 +6,9 @@ class Board
         # stores the value of each position in a 2d array
         # value is a number in the range [0, 6]
         @board = Array.new(Constant::NumOfBlocksY) {Array.new(Constant::NumOfBlocksX, 0)}
+
+        @game_lost = false
+        @score = 0
     end
 
     # generates a random block at the top of the screen
@@ -13,26 +16,57 @@ class Board
         random_number = rand(1..6)
 
         @cur_shape = 0 #placeholder value
+        @cur_shape_symbol = 0
 
         midX = Constant::NumOfBlocksX/2
-        midY = 0
+        midY = 1
         case random_number
         when 1
+            @cur_shape_symbol = :Box
             @cur_shape = Box.new(x: midX, y: midY)
         when 2
+            @cur_shape_symbol = :Jed
             @cur_shape = Jed.new(x: midX, y: midY)
         when 3
+            @cur_shape_symbol = :Led
             @cur_shape = Led.new(x: midX, y: midY)
         when 4
+            @cur_shape_symbol = :Stick
             @cur_shape = Stick.new(x: midX, y: midY)
         when 5
+            @cur_shape_symbol = :Tee
             @cur_shape = Tee.new(x: midX, y: midY)
         when 6
+            @cur_shape_symbol = :Zed
             @cur_shape = Zed.new(x: midX, y: midY)
         end
 
-        puts random_number
-        
+        puts @cur_shape_symbol
+
+        if !gameover?
+            add_shape(@cur_shape)
+        else
+            @game_lost = true
+        end
+
         @cur_shape
+    end
+
+    # checks for collision of the given shape with blocks on the board
+    def colliding?(shape)
+        shape.get_shape.each { |cur_block|
+            if @board[cur_block.getY][cur_block.getX] != 0
+                return true
+        }
+
+        return false
+    end
+
+    # checks if there is any collision with the roof i.e. checks if the game is over
+    def gameover?
+        if colliding?(@cur_shape)
+            return true
+        else
+            return false
     end
 end
